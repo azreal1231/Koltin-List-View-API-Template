c1f789088bfdd8890b3ddd206bc1750825af7c9a
azreal <edward@slatelight.co.za>
2021-07-21 14:23:56 +0200

    ? restructured MainActivity.kt to look better

--- app/src/main/java/com/slatelight/recyclerapi_example/MainActivity.kt
+++ app/src/main/java/com/slatelight/recyclerapi_example/MainActivity.kt
@@ -20,93 +20,89 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val listView: ListView =  listView_payment
-        var listView = findViewById<ListView>(R.id.listView_payment)
+        getTransactions(this)
+    }
+
+    private fun getTransactions(_ctx: Context){
+        try {
+            val innerParams = HashMap<String, String>()
+            innerParams["inner_param"] = "innerParamValue"
+
+            val outerParams = HashMap<String, String>()
+            outerParams["outer_param"] = "outerParamValue"
+
+
+            val outerJsonObject = JSONObject(outerParams as Map<*, *>)
+            val innerJsonObject = JSONObject(innerParams as Map<*, *>)
+            outerJsonObject.put("params", innerJsonObject)
+
+            val request = JsonObjectRequest(
+                Request.Method.POST, "https://wtfisthis.club/gettransaction", outerJsonObject,
+
+                { response ->
+                    try {
+                        val jsonArray: JSONArray = response.getJSONArray("data")
+                        makeRecyclerView(jsonArray, _ctx)
+                    } catch (e: Exception) {
+                        alert(this, "API Error")
+                        print(e)
+                    }
+
+                }, {
+                    alert(this, "Request TimeOut")
+                })
+            request.retryPolicy = DefaultRetryPolicy(
+                15000, // TimeOut
+                0, // DefaultRetryPolicy.DEFAULT_MAX_RETRIES = 2
+                1f // DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+            )
+
+            VolleySingleton.getInstance(_ctx).addToRequestQueue(request)
+
+        }catch (e: IOException) {
+            println(e)
+        }
+
+    }
+
+    private fun makeRecyclerView(_jsonArr: JSONArray, _ctx: Context) {
+
+        val listView = findViewById<ListView>(R.id.listView_payment)
 
         val list = mutableListOf<payment_history_Models>()
 
+        for (i in 0 until _jsonArr.length()){
+            val jsonObj = _jsonArr.getJSONObject(i)
+            val refId = jsonObj.getString("merchantReference")
 
-        fun json2(_jsonarr: JSONArray) {
-            print(_jsonarr)
+            payment_IDs.add(refId)
 
-            for (i in 0 until _jsonarr.length()){
-                var json_obj = _jsonarr.getJSONObject(i)
-                val ref_id = json_obj.getString("merchantReference")
-                payment_IDs.add(ref_id)
+            val status = jsonObj.getString("status")
+            val date = jsonObj.getString("date")
 
-                val status = json_obj.getString("status")
-                val date = json_obj.getString("date")
+            val amount = jsonObj.getString("totalAmount")
+            val amountLen = amount.length
+            val rands:String = amount.slice(0 until amountLen-2)     //Gets the rand value
+            val cents:String = amount.slice(amountLen-2 until amountLen)   //gets the cents value
 
-                val amount = json_obj.getString("totalAmount")
-                val amount_len = amount.length
-                val rands:String = amount.slice(0 until amount_len-2)     //Gets the rand value
-                val cents:String = amount.slice(amount_len-2 until amount_len)   //gets the cents value
+            val finalAmount = "R$rands.$cents"    //Creates final amount String
 
-                val finalAmount = "R$rands.$cents"    //Creates final amount String
-
-
-
-                if (status == "completed"){
-                    list.add(payment_history_Models(status, date, finalAmount, R.drawable.checked_green))
-                }
-                else{
-                    list.add(payment_history_Models(status, date, finalAmount, R.drawable.alert_red))
-                }
+            if (status == "completed"){
+                list.add(payment_history_Models(status, date, finalAmount, R.drawable.checked_green))
             }
-
-            val adapt = paymentHistoryAdapter(this, R.layout.payment_history_row, list)
-            listView.adapter = adapt
-
-
-            listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-                alert(this, "ID: ${payment_IDs[position]}")
+            else{
+                list.add(payment_history_Models(status, date, finalAmount, R.drawable.alert_red))
             }
         }
 
-        fun read_json(_ctx: Context) {
-            val json : String? = null
-            try {
-                val innerParams = HashMap<String, String>()
-                innerParams["WorkflowID"] = ""
-
-                val params = HashMap<String, String>()
-                params["de"] = "PrevalidatePYPCollection"
+        val adapt = paymentHistoryAdapter(this, R.layout.payment_history_row, list)
+        listView.adapter = adapt
 
 
-                val jsonObject = JSONObject(params as Map<*, *>)
-                val bleh = JSONObject(innerParams as Map<*, *>)
-                jsonObject.put("params", bleh)
-
-                val request = JsonObjectRequest(
-                    Request.Method.POST, "https://wtfisthis.club/gettransaction", jsonObject,
-
-                    { response ->
-                        try {
-                            val jsonArray: JSONArray = response.getJSONArray("data")
-                            json2(jsonArray)
-                        } catch (e: Exception) {
-                            alert(this, "Error with BM Server")
-                        }
-
-                    }, {
-                        alert(this, "Request TimeOut")
-                    })
-                request.retryPolicy = DefaultRetryPolicy(
-                    15000, // TimeOut
-                    0, // DefaultRetryPolicy.DEFAULT_MAX_RETRIES = 2
-                    1f // DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
-                )
-
-                val contect = _ctx
-                VolleySingleton.getInstance(contect).addToRequestQueue(request)
-
-            }catch (e: IOException) {
-                println(e)
-            }
-
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            alert(this, "ID: ${payment_IDs[position]}")
         }
-
-        read_json(applicationContext)
-//        hello
     }
+
+
 }
